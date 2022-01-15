56c399ffe7916261ca71adef6749b15c8497b46d
kavinda96 <kavindasg@gmail.com>
2022-01-15 15:46:46 +0300

    first commit

---
+++ src/test/java/com/demo/coinbase/CoinbaseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.demo.coinbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test initiate");
+	}
+
+}
